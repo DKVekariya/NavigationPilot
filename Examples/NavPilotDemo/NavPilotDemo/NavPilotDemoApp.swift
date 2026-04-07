37b432a586ccf424f3d2634dbb6f6ba1efb7f817
Divyesh Vekariya <divyeshkvekariya@gmail.com>
2026-04-07 10:21:30 +0530

    Initial commit

---
+++ Examples/NavPilotDemo/NavPilotDemo/NavPilotDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavPilotDemoApp.swift
+//  NavPilotDemo
+//
+//  Created by DK on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NavPilotDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/NavPilotDemo/NavPilotDemoTests/NavPilotDemoTests.swift
@@ -0,0 +1,18 @@
+//
+//  NavPilotDemoTests.swift
+//  NavPilotDemoTests
+//
+//  Created by DK on 07/04/26.
+//
+
+import Testing
+
+struct NavPilotDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
